6d569ab1b65c7cb6294c4d1ce4b47b8ab3c028f9
Adrien <aburillon@gmail.com>
2013-10-06 15:32:54 +0200

    Clothes.java : Modification de la visibilité des attributs (protected), et du type de WeatherList + ajout d'un attribut label + generation Getters/Setters

--- src/main/java/fr/redteam/dressyourself/core/clothes/Clothes.java
+++ src/main/java/fr/redteam/dressyourself/core/clothes/Clothes.java
@@ -1,6 +1,7 @@
 package main.java.fr.redteam.dressyourself.core.clothes;
+
+import java.util.List;
 import java.util.Set;
-import java.util.HashSet;
 
 
 /**
@@ -18,7 +19,7 @@ public abstract class Clothes
 	 * @ordered
 	 */
 	
-	public Colors color;
+	protected String label;
 	
 	/**
 	 * <!-- begin-user-doc -->
@@ -27,7 +28,7 @@ public abstract class Clothes
 	 * @ordered
 	 */
 	
-	public Weather weatherList;
+	protected Colors color;
 	
 	/**
 	 * <!-- begin-user-doc -->
@@ -36,7 +37,7 @@ public abstract class Clothes
 	 * @ordered
 	 */
 	
-	public String description;
+	protected List<Weather> weatherList;
 	
 	/**
 	 * <!-- begin-user-doc -->
@@ -45,7 +46,16 @@ public abstract class Clothes
 	 * @ordered
 	 */
 	
-	public String imageUrl;
+	protected String description;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	protected String imageUrl;
 	
 	/**
 	 * <!-- begin-user-doc -->
@@ -65,5 +75,76 @@ public abstract class Clothes
 		super();
 	}
 
-}
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
 
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Colors getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Colors color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the weatherList
+	 */
+	public List<Weather> getWeatherList() {
+		return weatherList;
+	}
+
+	/**
+	 * @param weatherList the weatherList to set
+	 */
+	public void setWeatherList(List<Weather> weatherList) {
+		this.weatherList = weatherList;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the imageUrl
+	 */
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	/**
+	 * @param imageUrl the imageUrl to set
+	 */
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	
+	
+}
