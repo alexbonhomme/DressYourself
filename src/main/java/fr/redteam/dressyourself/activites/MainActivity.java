543cff1fc73b21ed3c7d19fdbf67de88ced1c710
antonia <antonia.ludunge@gmail.com>
2013-10-08 01:16:29 +0200

    Génération page tenue

--- src/main/java/fr/redteam/dressyourself/activites/MainActivity.java
+++ src/main/java/fr/redteam/dressyourself/activites/MainActivity.java
@@ -1,11 +1,13 @@
 package main.java.fr.redteam.dressyourself.activites;
 
 import main.java.fr.redteam.dressyourself.R;
-import main.java.fr.redteam.dressyourself.R.layout;
-import main.java.fr.redteam.dressyourself.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +15,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button connection = (Button) findViewById(R.id.but_test);
+	      connection.setOnClickListener(new OnClickListener() {
+	      			
+	    @Override
+	    public void onClick(View v) {
+	    	Intent intent = new Intent(MainActivity.this, OutfitActivity.class);
+	        startActivity(intent);
+	      	}
+	      });
 	}
 
 	@Override
