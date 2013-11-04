485c09eb9ead9fded1ef1e7d5b5261ef904e17ee
Jérémy <Dalarad.runebringer@gmail.com>
2013-11-04 15:27:45 +0100

    ActivityDetailClotheTest : Ajout des tests

--- tests/main/java/fr/redteam/dressyourselftest/activitesTest/ActivityClotheDetailTest.java
+++ tests/main/java/fr/redteam/dressyourselftest/activitesTest/ActivityClotheDetailTest.java
@@ -1,17 +1,28 @@
 package main.java.fr.redteam.dressyourselftest.activitesTest;
 
+import java.util.List;
+
+import main.java.fr.redteam.dressyourself.R;
 import main.java.fr.redteam.dressyourself.activites.ActivityClotheDetail;
+import main.java.fr.redteam.dressyourself.common.DBHelper;
+import main.java.fr.redteam.dressyourself.core.clothes.Clothe;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
 public class ActivityClotheDetailTest extends ActivityInstrumentationTestCase2<ActivityClotheDetail> {
 	
 	  private ActivityClotheDetail activityClotheDetail;
-
+	  private TextView textViewBrand;
+	  private TextView textViewModel;
+	  private TextView textViewColor;
+	  private TextView textViewType;
+	  private TextView textViewWeather;
+	  private Clothe VetementTest;
 	  
 	  public ActivityClotheDetailTest(Class<ActivityClotheDetail> activityClass)
 	  {
 	    super(activityClass);
-
+	    this.VetementTest = new DBHelper(this.activityClotheDetail).getClothe(1);
 	  }
 
 	  public ActivityClotheDetailTest() {
@@ -24,7 +35,84 @@ public class ActivityClotheDetailTest extends ActivityInstrumentationTestCase2<A
 
 		    setActivityInitialTouchMode(false);
 		    activityClotheDetail = getActivity();
+		    // Recup. du logo par exemple
+		    this.textViewBrand = (TextView) activityClotheDetail.findViewById(R.id.bodyTxt);
+		    this.textViewModel = (TextView) activityClotheDetail.findViewById(R.id.LabelTxt);
+		    this.textViewColor = (TextView) activityClotheDetail.findViewById(R.id.colorTxt);
+		    this.textViewWeather = (TextView) activityClotheDetail.findViewById(R.id.weatherTxt);
+		    this.textViewType = (TextView) activityClotheDetail.findViewById(R.id.TypeTxt);
+	  }
+	  
+	  /*
+	   * Check the content of textViewBody 
+	   */
+	  public void textViewBodyTestValue()
+	  {
+		  assertEquals(textViewBrand.getText(),VetementTest.getBrand());
+	  }
+	  
 
+	  public void textViewBodyTestNotNull()
+	  {
+		  assertFalse(this.textViewBrand.getText() == null || this.textViewBrand.getText().equals(""));
+	  }
+	  
+	  
+	  /*
+	   * Check the content of textViewLabel
+	   */
+	  public void textViewLabelTestValue()
+	  {
+		  assertEquals(textViewModel.getText(),VetementTest.getModel());
+	  }
+	  
+	  public void textViewLabelTestNotNull()
+	  {
+		  assertFalse(this.textViewModel.getText() == null || this.textViewModel.getText().equals(""));
+	  }
+	  
+	  /*
+	   * Check the content of textViewLabel
+	   */
+	  public void textViewColorTestValue()
+	  {
+		  assertEquals(textViewColor.getText(),VetementTest.getColor());
+	  }
+	  
+	  public void textViewColorTestNotNull()
+	  {
+		  assertFalse(this.textViewColor.getText() == null || this.textViewColor.getText().equals(""));
+	  }
+	  
+	  /*
+	   * Check the content of textViewWeather
+	   */
+	  public void textViewWeatherTestValue()
+	  {
+		    List<String> TheWeather = VetementTest.getWeather();
+		    String WeatherTxt = "";
+		    for (String weatherLine : TheWeather) {
+		      WeatherTxt += weatherLine + " ";
+		    }
+		    assertEquals(this.textViewWeather.getText(),WeatherTxt);
+	  }
+	  
+	  public void textViewWeatherTestNotNull()
+	  {
+		    assertFalse(this.textViewWeather.getText() == null || this.textViewWeather.getText().equals(""));
+	  }
+	  
+	  /*
+	   * Check the content of textViewType
+	   */
+	  public void textViewTypeTestValue()
+	  {
+		  assertEquals(textViewType.getText(),VetementTest.getType());
+	  }
+	  
+	  public void textViewTypeTestNotNull()
+	  {
+		  assertFalse(this.textViewType.getText() == null || this.textViewType.getText().equals(""));
 	  }
 
 
