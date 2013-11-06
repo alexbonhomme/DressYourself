04e80091ba02fcca4565bb7e29a65000b532ae63
blckshrk <alexand.bonhom@gmail.com>
2013-11-06 01:48:07 +0100

    DY-46 : Implémentation d'un premier test afin de verifier la récupération de donné a partir d'une url. Cette methote est utilisé pour la communication avec l'API

---
+++ test/main/java/fr/redteam/dressyourself/core/api/APIAbstractHelperTest.java
@@ -0,0 +1,26 @@
+package main.java.fr.redteam.dressyourself.core.api;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Alexandre Bonhomme
+ * 
+ */
+public class APIAbstractHelperTest {
+
+  @Test
+  public void testGetContent() throws MalformedURLException {
+    APIAbstractHelper api = new APIAbstractHelper() {};
+
+    URL url = new URL("http://ip.jsontest.com/");
+    String content = api.getContent(url);
+
+    assertEquals("{\"ip\": \"2.5.185.37\"}", content);
+  }
+}
