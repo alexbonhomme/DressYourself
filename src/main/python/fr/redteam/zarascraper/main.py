9dd69e21fc747e7f50049ab30ce052a1fb4c9f8e
blckshrk <alexand.bonhom@gmail.com>
2013-11-10 15:06:42 +0100

    DY-46 : Création d'un répertoire src/main/python/ pour accueillir le code source du scraper. Celui-ci ayant pour but de construire une db à partir de données récoltées depuis le site zara.com

---
+++ src/main/python/fr/redteam/zarascraper/main.py
@@ -0,0 +1,93 @@
+'''
+Created on 24 oct. 2013
+
+@author: Alexandre Bonhomme
+'''
+from fr.redteam.zarascraper.core.dbhelper import DBHelper
+from fr.redteam.zarascraper.scrapers.zara.zara_scraper import ZaraScrape
+import logging as log
+import os
+import sys
+
+class Main(object):
+
+    SQL_DATABASE_PATH = 'dressyourself.sqlite'
+
+    def __init__(self):
+        self.scraper = ZaraScrape('en')
+
+        '''
+        log.info('-- Removing old database --')
+        try:
+            os.remove(self.SQL_DATABASE_PATH)
+        except:
+            pass
+        '''
+
+        self.db = DBHelper(self.SQL_DATABASE_PATH)
+        self.db.open()
+        self.db.createDataBaseTablesIfNotExists()
+        self.db.close()
+
+    '''
+    Run scraping and fills the database
+    '''
+    def run(self):
+        self.scraper.setConfig('man', 'jeans', 'Jean', 'Bottom')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('man', 'sweatshirts', 'Sweat-shirt', 'Top')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('man', 'knitwear', 'Knitwears', 'Top')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('man', 'shoes', 'Shoes', 'Shoes')
+        itemList = self.scraper.run(usePlainImage = False);
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('woman', 'jeans', 'Jean', 'Bottom')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('woman', 'skirts', 'Skirt', 'Bottom')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('woman', 'knitwear', 'Knitwears', 'Top')
+        itemList = self.scraper.run();
+        self.fillDataBase(itemList)
+
+        self.scraper.setConfig('woman', 'shoes', 'Shoes', 'Shoes')
+        itemList = self.scraper.run(usePlainImage = False);
+        self.fillDataBase(itemList)
+
+    '''
+    Fills database with products from scraping
+    '''
+    def fillDataBase(self, productList):
+        self.db.open()
+
+        log.info('-- Starting insertions to database --')
+        for product in productList:
+            self.db.insertProduct(product)
+
+        self.db.close()
+
+'''
+Main
+'''
+if __name__ == '__main__':
+    log.basicConfig(level = log.DEBUG)
+
+    '''
+    if len(sys.argv) < 2:
+        print('Usage: ' + sys.argv[0] + ' section subsection')
+    else:
+        Main('en', sys.argv[1], sys.argv[2]).run()
+    '''
+    Main().run()
+
---
+++ src/main/python/fr/redteam/zarascraper/scrapers/zara/zara_scraper.py
@@ -0,0 +1,93 @@
+'''
+Created on 7 nov. 2013
+
+@author: Alexandre Bonhomme
+'''
+from fr.redteam.zarascraper.core.downloader import Downloader
+from fr.redteam.zarascraper.core.product import Product
+from fr.redteam.zarascraper.scrapers.scraper import Scraper
+from fr.redteam.zarascraper.scrapers.zara.zara_browser import ZaraBrowser
+import errno
+import logging as log
+import os
+import time
+
+class ZaraScrape(Scraper):
+
+    BRAND_NAME = 'Zara'
+    PAGE_BASE = 'http://www.zara.com/fr/'
+
+    def __init__(self, lang):
+        self.lang = lang
+        self.downloader = Downloader()
+
+    def setConfig(self, section, subsection, productType, bodyPart):
+        self.section = section
+        self.subsection = subsection
+        self.type = productType
+        self.bodies = bodyPart
+
+    '''
+        Perfom the scraping on Zara website
+    '''
+    def run(self, usePlainImage = True, download = False):
+        if download:
+            self.dl_folder = self.DL_FOLDER_PATH_BASE + self.lang + '/' + self.section + '/' + self.subsection + '/'
+            # Create folder if is not existing
+            try:
+                os.makedirs(self.dl_folder)
+            except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
+
+        log.info('-- Starting scraping --')
+
+        home = self.downloader.getFile(self.PAGE_BASE + self.lang + '/')
+        browser = ZaraBrowser(home)
+
+        url = browser.getMenuLinkFromName(self.section)
+        try:
+            browser.goTo(url, 5)
+        except:
+            log.warning("Unable to get the page '" + url + "'. Omitting.")
+            return []
+
+        url = browser.getMenuLinkFromName(self.subsection)
+        try:
+            browser.goTo(url, 5)
+        except:
+            log.warning("Unable to get the page '" + url + "'. Omitting.")
+            return []
+
+        i = 0
+        itemList = []
+        for item in browser.getProductsList():
+            log.debug('zzZZZZzzz')
+            time.sleep(3) # let's do it cool
+
+            try:
+                browser.goTo(item['url'])
+            except:
+                log.warning("FAIL : Unable to download '" + item['name'] + "'. Omitting.")
+                continue
+
+            imgUrl = browser.getProductImageLink(usePlainImage)
+            if imgUrl is None:
+                log.info('FAIL : Unable to get product image for "' + item['name'] + '". Omitting.')
+                continue
+
+            if download:
+                imgFilename = str(i) + '-' + item['name']
+                log.info('Downloading ' + imgFilename + '...')
+                self.downloader.writeFile(imgUrl, self.dl_folder + imgFilename)
+
+            color = browser.getProductColor()
+
+            log.info('SUCCES : Product "' + item['name'] + '" scraping done.')
+            itemList.append(Product(item['name'], self.BRAND_NAME, color, imgUrl, self.type, self.bodies))
+            i += 1
+
+        log.info('-- Ending scraping --')
+        log.info('-- ' + str(i) + ' images was scraped --')
+
+        return itemList
