8d2b0b570bd81aefc4bbf2912f0d0a8339bdd87e
dalarad <jerm8@free.fr>
2013-11-11 16:54:05 +0100

    DY-39: Factorisation du code d'envoie de mail en un Plugin.
    -> MailPlugin: - classe mère
    -> MailOutfitPlugin : classe destiné à la création de mail d'une tenue
    hérite de MailPlugin
    -> MailOutfitPlugin : classe destiné à la création de mail d'un vetement
    hérite de MailPlugin

--- src/main/java/fr/redteam/dressyourself/activities/ActivityClotheMail.java
+++ src/main/java/fr/redteam/dressyourself/activities/ActivityClotheMail.java
@@ -1,19 +1,14 @@
 package fr.redteam.dressyourself.activities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import fr.redteam.dressyourself.R;
-import fr.redteam.dressyourself.common.DBHelper;
-import fr.redteam.dressyourself.core.clothes.Clothe;
+import fr.redteam.dressyourself.plugins.MailClothePlugin;
 
 public class ActivityClotheMail extends Activity {
 
@@ -43,69 +38,16 @@ public class ActivityClotheMail extends Activity {
     });
   }
 
-  public String[] ListDestinataire() {
-    String txtDsc = this.textDestinataire.getText().toString();
-    String[] DscList;
-    int nbDst = 0;
-    List<Integer> positionSeparateur = new ArrayList<Integer>();
-    for (int i = 0; i < txtDsc.length(); i++) {
-      if (txtDsc.charAt(i) == ';') {
-        nbDst++;
-        positionSeparateur.add(i);
-      }
-    }
-
-    if (nbDst > 0) {
-      int depart = 0;
-      DscList = new String[nbDst + 1];
-      for (int i = 0; i < nbDst; i++) {
-        DscList[i] = txtDsc.substring(depart, positionSeparateur.get(i));
-        depart = positionSeparateur.get(i) + 1;
-      }
-      DscList[nbDst] = txtDsc.substring(positionSeparateur.get(nbDst - 1) + 1);
-    } else {
-      DscList = new String[1];
-      DscList[0] = txtDsc;
-    }
-    return DscList;
-  }
-
   /**
    * function which made an mail intent in order to send it.
    */
 
   public void creationMail() {
-    Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-    Clothe vetement;
-    /**
-     * get the key of outfit which was passed in parameter.
-     */
-    Bundle b = getIntent().getExtras();
-    int cleTenue = b.getInt("idClothe");
-
-    vetement = new DBHelper(this).getClothe(cleTenue);
-
-    /* Set the type of the mail */
-    emailIntent.setType("image/png");
-    /* add destinaire */
-    emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, this.ListDestinataire());
-    /* Add the subject for the mail */
-    emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Partage d'une tenue");
-    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, this.textContenu.getText().toString()
-        + "\n");
-    /* Add text */
-    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-        "j'ai trouvé cette tenue et je pense qu'elle va te plaire.\n");
-    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-        "Ma tenue contient les pieces suivantes.\n");
-    /* Made the body of mail */
-    emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "un(e) " + vetement.getType()
-        + " de la marque " + vetement.getBrand() + " et de couleur " + vetement.getColor() + " "
-        + vetement.getModel() + ".\n");
-    emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + vetement.getImage()));
-
-    startActivity(Intent.createChooser(emailIntent, "Choisissez un client mail"));
-
+    MailClothePlugin mail =
+        new MailClothePlugin(getIntent().getExtras().getInt("idClothe"),
+            "i want to share this Outfit", this.textContenu.getText().toString(),
+            this.textDestinataire.getText().toString(), this);
+    mail.creationMail();
   }
 
 }
