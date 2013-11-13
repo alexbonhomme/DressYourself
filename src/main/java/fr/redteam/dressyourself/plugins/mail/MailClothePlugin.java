3e53da93f6384e7bff36a2b386514e7685e3cb8a
dalarad <jerm8@free.fr>
2013-11-13 02:22:05 +0100

    Dy-39 : Correction du plugin Mail pour qu'on envoie un outfit ou un clothe en place de l'int de clé primaire.

--- src/main/java/fr/redteam/dressyourself/plugins/mail/MailClothePlugin.java
+++ src/main/java/fr/redteam/dressyourself/plugins/mail/MailClothePlugin.java
@@ -1,29 +1,28 @@
 package fr.redteam.dressyourself.plugins.mail;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import fr.redteam.dressyourself.common.DBHelper;
 import fr.redteam.dressyourself.core.clothes.Clothe;
 
 public class MailClothePlugin extends MailPlugin {
 
-  public MailClothePlugin(int id, String subject, String textBody, String textDestinataire,
+  private Clothe clothe;
+
+  public MailClothePlugin(Clothe clothe, String subject, String textBody, String textDestinataire,
       Activity activity) {
-    super(id, subject, textBody, textDestinataire, activity);
+    super(subject, textBody, textDestinataire, activity);
+    this.clothe = clothe;
   }
 
   protected void Body() {
 
-    Clothe vetement = new DBHelper(this.activity).getClothe(this.id);
     /* Add text */
     mailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-        "j'ai trouvÃ© ce vetement et je pense qu'il va te plaire.\n");
+        "j'ai trouvé ce vetement et je pense qu'il va te plaire.\n");
     /* Made the body of mail */
-    mailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "un(e) " + vetement.getType()
-        + " de la marque " + vetement.getBrand() + " et de couleur " + vetement.getColor() + " "
-        + vetement.getModel() + ".\n");
-    mailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + vetement.getImage()));
+    mailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "un(e) " + this.clothe.getType()
+        + " de la marque " + this.clothe.getBrand() + " et de couleur " + this.clothe.getColor()
+        + " " + this.clothe.getModel() + ".\n");
+    // mailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + this.clothe.getImage()));
   }
 
 }
