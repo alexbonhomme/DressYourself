903b8e335926c6d6b065f2c025f2d6c8e68766a1
blckshrk <alexand.bonhom@gmail.com>
2013-11-20 21:23:43 +0100

    Implémentation d'un test simple sur la page ActivitySearchEngine

---
+++ src/test/java/fr/redteam/dressyourself/activities/ActivitySearchEngineTest.java
@@ -0,0 +1,40 @@
+package fr.redteam.dressyourself.activities;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import android.app.SearchManager;
+import android.content.Context;
+
+/**
+ * 
+ * @author Alexandre Bonhomme
+ * 
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ActivitySearchEngineTest {
+
+  ActivitySearchEngine searchActivity;
+  Context context;
+
+  @Before
+  public void setUp() {
+    searchActivity = Robolectric.buildActivity(ActivitySearchEngine.class).create().visible().get();
+    context = Robolectric.getShadowApplication().getApplicationContext();
+  }
+
+  @Test
+  public void testSearchManager() throws Exception {
+    SearchManager searchManager = (SearchManager) context.getSystemService(Context.SEARCH_SERVICE);
+    assertNotNull(searchManager);
+
+    searchManager.startSearch("test search string", true, searchActivity.getComponentName(), null,
+        false);
+    searchManager.stopSearch();
+  }
+}
