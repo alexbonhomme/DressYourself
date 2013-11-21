3a01085a434429aba2783be41a2660d3ee66c07f
frecourt <frecourt@bitbucket.org>
2013-11-21 16:02:25 +0100

    DY-49 gestion de la liste vide

--- src/main/java/fr/redteam/dressyourself/activities/ActivityClotheList.java
+++ src/main/java/fr/redteam/dressyourself/activities/ActivityClotheList.java
@@ -12,35 +12,45 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 //import fr.redteam.dressyourself.R;
-import android.widget.Toast;
 
 public class ActivityClotheList extends ListActivity {
 
 	private ArrayList<Clothe> clotheList;
+	private boolean listIsEmpty = false;
 
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		String[] values;
 		DBHelper db = new DBHelper(this);
 		db.open();
 		this.clotheList = db.getListClothes();
-		String[] values = new String[this.clotheList.size()];
-		int index = 0;
 		db.close();
 
-		for(Clothe clothe : this.clotheList){
-			values[index] = clothe.getModel();
-			index++;
+		if(this.clotheList.size() == 0){
+			this.listIsEmpty = true; 
+			values = new String[]{"You don't have any clothe"};
+		}else{
+			values = new String[this.clotheList.size()];
+			int index = 0;
+
+			for(Clothe clothe : this.clotheList){
+				values[index] = clothe.getModel();
+				index++;
+			}
 		}
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, values);
 		setListAdapter(adapter);
+
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Intent intent = new Intent(ActivityClotheList.this, ActivityClotheDetail.class);
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("clothe", this.clotheList.get(position));
-        intent.putExtras(bundle);
-        startActivity(intent);
+		if(!this.listIsEmpty){
+			Intent intent = new Intent(ActivityClotheList.this, ActivityClotheDetail.class);
+			Bundle bundle = new Bundle();
+			bundle.putSerializable("clothe", this.clotheList.get(position));
+			intent.putExtras(bundle);
+			startActivity(intent);
+		}
 	}
 } 
