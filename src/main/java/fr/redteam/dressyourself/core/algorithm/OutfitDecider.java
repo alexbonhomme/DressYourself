5d3fbfb38969a9c819b5192b478b0a1a058367d3
boens <boens.remi@gmail.com>
2013-11-26 17:21:59 +0100

    DY-41 : Ajout de la partie de l'algorithme concernant la météo dans OutfitDecider

--- src/main/java/fr/redteam/dressyourself/core/algorithm/OutfitDecider.java
+++ src/main/java/fr/redteam/dressyourself/core/algorithm/OutfitDecider.java
@@ -1,9 +1,12 @@
 package fr.redteam.dressyourself.core.algorithm;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import android.util.Log;
 import fr.redteam.dressyourself.core.clothes.Clothe;
+import fr.redteam.dressyourself.plugins.weather.Weather;
 
 
 /**
@@ -34,49 +37,107 @@ public class OutfitDecider {
 
   public Clothe decideTop(List<Clothe> listTop) {
     int randomInt;
+    List<Clothe> listTopWeather = new ArrayList<Clothe>();
 
     if (!weather) {
       randomInt = randomGenerator.nextInt(listTop.size());
+
       while (randomInt == currentTop) {
         randomInt = randomGenerator.nextInt(listTop.size());
       }
       currentTop = randomInt;
+
       return listTop.get(currentTop);
     } else {
-      // TODO prendre en compte la météo
-      return listTop.get(0);
+      listTopWeather = buildListWeather(listTop);
+      if (listTopWeather.size() > 0) {
+        randomInt = randomGenerator.nextInt(listTopWeather.size());
+
+        while (randomInt == currentTop) {
+          randomInt = randomGenerator.nextInt(listTopWeather.size());
+        }
+        currentTop = randomInt;
+
+        return listTopWeather.get(currentTop);
+      }
     }
+    Log.d("erreur", "decideTop could not decide");
+    return listTop.get(0);
   }
 
   public Clothe decideBottom(List<Clothe> listBottom) {
     int randomInt;
+    List<Clothe> listBottomWeather = new ArrayList<Clothe>();
 
     if (!weather) {
       randomInt = randomGenerator.nextInt(listBottom.size());
-      while (randomInt == currentBottom) {
+
+      while (randomInt == currentTop) {
         randomInt = randomGenerator.nextInt(listBottom.size());
       }
       currentBottom = randomInt;
+
       return listBottom.get(currentBottom);
     } else {
-      // TODO prendre en compte la météo
-      return listBottom.get(0);
+      listBottomWeather = buildListWeather(listBottom);
+      if (listBottomWeather.size() > 0) {
+        randomInt = randomGenerator.nextInt(listBottomWeather.size());
+
+        while (randomInt == currentTop) {
+          randomInt = randomGenerator.nextInt(listBottomWeather.size());
+        }
+        currentBottom = randomInt;
+
+        return listBottomWeather.get(currentBottom);
+      }
     }
+    Log.d("erreur", "decideBottom could not decide");
+    return listBottom.get(0);
   }
 
   public Clothe decideFeet(List<Clothe> listFeet) {
     int randomInt;
+    List<Clothe> listFeetWeather = new ArrayList<Clothe>();
 
     if (!weather) {
       randomInt = randomGenerator.nextInt(listFeet.size());
+
       while (randomInt == currentFeet) {
         randomInt = randomGenerator.nextInt(listFeet.size());
       }
       currentFeet = randomInt;
+
       return listFeet.get(currentFeet);
     } else {
-      // TODO prendre en compte la météo
-      return listFeet.get(0);
+      listFeetWeather = buildListWeather(listFeet);
+      if (listFeetWeather.size() > 0) {
+        randomInt = randomGenerator.nextInt(listFeetWeather.size());
+
+        while (randomInt == currentFeet) {
+          randomInt = randomGenerator.nextInt(listFeetWeather.size());
+        }
+        currentFeet = randomInt;
+
+        return listFeetWeather.get(currentFeet);
+      }
     }
+    Log.d("erreur", "decideFeet could not decide");
+    return listFeet.get(0);
+  }
+
+  // build a new list with only clothes adapted for the current weather
+  private List<Clothe> buildListWeather(List<Clothe> listClothe) {
+    List<Clothe> listClotheWeather = new ArrayList<Clothe>();
+
+    for (Clothe clothe : listClothe) {
+      List<String> listWeather = clothe.getWeather();
+      for (String string : listWeather) {
+        if (string.equals(Weather.getWeather())) {
+          listClotheWeather.add(clothe);
+        }
+      }
+    }
+
+    return listClotheWeather;
   }
 }
