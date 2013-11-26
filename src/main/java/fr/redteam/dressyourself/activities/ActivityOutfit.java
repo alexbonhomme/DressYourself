69f13683aec9f9e6901b8d8a0fc3521ff8375eab
antonia <antonia.ludunge@gmail.com>
2013-11-26 12:45:09 +0100

    Refactoring de code, factorisation de différents traitements dans des méthodes afin de rendre le code plus lisible

--- src/main/java/fr/redteam/dressyourself/activities/ActivityOutfit.java
+++ src/main/java/fr/redteam/dressyourself/activities/ActivityOutfit.java
@@ -41,45 +41,54 @@ public class ActivityOutfit extends Activity implements OnClickListener {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_outfit_layout);
-    textViewTop = (TextView) findViewById(R.id.textview_top);
-    textViewBottom = (TextView) findViewById(R.id.textview_bottom);
-    textViewFeet = (TextView) findViewById(R.id.textview_feet);
-    imageTop = (ImageView) findViewById(R.id.imageview_top);
-    imageBottom = (ImageView) findViewById(R.id.imageview_bottom);
-    imageFeet = (ImageView) findViewById(R.id.imageview_feet);
-    
+    setContentView(R.layout.activity_outfit_layout); 
+    initComponent();
     if (Weather.getWeather() != null) {
       Log.d("weather", Weather.getWeather());
     }
-
     // connexion bdd
     db.open();
-
-    // recup top
-    listTop = db.getListTop();
-
-    // recup bottom
-    listBottom = db.getListBottom();
-
-    // recup feet
-    listFeet = db.getListFeet();
-
-    // vetements statique TODO: récupérer en bdd
-    Clothe clothe = new Clothe("Pull beige");
-    textViewTop.setText(clothe.getModel());
-    textViewBottom.setText("slim bleu fonce");
-    textViewFeet.setText("Basket camel");
-
-    buttonRefreshTop = (Button) findViewById(R.id.button_refresh_top);
-    buttonRefreshTop.setOnClickListener(this);
-
-    buttonRefreshBottom = (Button) findViewById(R.id.button_refresh_bottom);
-    buttonRefreshBottom.setOnClickListener(this);
-
-    buttonRefreshFeet = (Button) findViewById(R.id.button_refresh_feet);
-    buttonRefreshFeet.setOnClickListener(this);
+    getData();
+    bindToLayout();
+    setListener();
     db.close();
+  }
+  
+  private void initComponent() {
+	   textViewTop = (TextView) findViewById(R.id.textview_top);
+	   textViewBottom = (TextView) findViewById(R.id.textview_bottom);
+	   textViewFeet = (TextView) findViewById(R.id.textview_feet);
+	   imageTop = (ImageView) findViewById(R.id.imageview_top);
+	   imageBottom = (ImageView) findViewById(R.id.imageview_bottom);
+	   imageFeet = (ImageView) findViewById(R.id.imageview_feet);
+	   buttonRefreshTop = (Button) findViewById(R.id.button_refresh_top);
+	   buttonRefreshBottom = (Button) findViewById(R.id.button_refresh_bottom);
+	   buttonRefreshFeet = (Button) findViewById(R.id.button_refresh_feet);
+  }
+  
+  private void getData() {
+	  // recup top
+	    listTop = db.getListTop();
+
+	    // recup bottom
+	    listBottom = db.getListBottom();
+
+	    // recup feet
+	    listFeet = db.getListFeet(); 
+  }
+  
+  private void bindToLayout() {
+	 // vetements statique TODO: récupérer en bdd
+	 Clothe clothe = new Clothe("Pull beige");
+	 textViewTop.setText(clothe.getModel());
+	 textViewBottom.setText("slim bleu fonce");
+	 textViewFeet.setText("Basket camel");	
+  }
+   
+  private void setListener() {
+	  buttonRefreshTop.setOnClickListener(this); 
+	  buttonRefreshBottom.setOnClickListener(this);
+	  buttonRefreshFeet.setOnClickListener(this);
   }
 
   private void refreshTop() {
