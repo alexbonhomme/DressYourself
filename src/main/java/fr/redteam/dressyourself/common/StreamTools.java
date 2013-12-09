daddd57b217b39d6ec2bceee5b3713811e15a261
blckshrk <alexand.bonhom@gmail.com>
2013-12-09 15:31:29 +0100

    DY-46 Suppression de la fonction readStream() qui n'est pas utilisée dans le projet.

--- src/main/java/fr/redteam/dressyourself/common/StreamTools.java
+++ src/main/java/fr/redteam/dressyourself/common/StreamTools.java
@@ -1,46 +1,16 @@
 package fr.redteam.dressyourself.common;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import android.util.Log;
 
 public class StreamTools {
-
-	static public String readStream(InputStream in) {
-		String out = "";
-		
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new InputStreamReader(in));
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				out += line + "\n";
-			}
-
-		} catch (IOException e) {
-			Log.w("Stream IO", e.getMessage());
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					Log.w("Stream IO", e.getMessage());
-				}
-			}
-		}
 	
-		return out;
-	}
-	
-	/**
-	 * Source : http://stackoverflow.com/a/5445161
-	 * @param is
-	 * @return
-	 */
-	public static String convertStreamToString(java.io.InputStream is) {
+  /**
+   * Convert an InputStream into a String object
+   * 
+   * Source : http://stackoverflow.com/a/5445161
+   */
+  public static String convertStreamToString(InputStream is) {
 	    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
 	    return s.hasNext() ? s.next() : "";
 	}
