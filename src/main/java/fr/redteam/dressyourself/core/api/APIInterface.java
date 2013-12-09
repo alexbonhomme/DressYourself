93af8303e6a9a092bad5aac67f4ac4e3538d9326
blckshrk <alexand.bonhom@gmail.com>
2013-12-09 15:39:01 +0100

    DY-46 Amélioration de la javadoc de la classe APIInterface (ajout de lien entre les classes)

--- src/main/java/fr/redteam/dressyourself/core/api/APIInterface.java
+++ src/main/java/fr/redteam/dressyourself/core/api/APIInterface.java
@@ -16,42 +16,44 @@ import fr.redteam.dressyourself.core.clothes.Clothe;
 public interface APIInterface {
 
   /**
-   * Return a `Clothe` object from is ID in the database API
+   * Return a {@link Clothe Clothe} object from is ID in the database API
    * 
    * @param id ID of the product in the remote database
    */
   Clothe findClotheById(int id);
 
   /**
-   * Return a list of `Clothe` objects when their names contains `model`
+   * Return a list of {@link Clothe Clothe} objects when their names contains <code>model</code>
    */
   List<Clothe> findClothesByModel(String model);
 
   /**
-   * Return a list of `Clothe` objects when their brand contains `brand`
+   * Return a list of {@link Clothe Clothe} objects when their brand contains <code>brand</code>
    */
   List<Clothe> findClothesByBrand(String brand);
 
   /**
-   * Return a list of `Clothe` objects when their color contains `color`
+   * Return a list of {@link Clothe Clothe} objects when their color contains <code>color</code>
    */
   List<Clothe> findClothesByColor(String color);
 
   /**
-   * Return a list of `Clothe` objects when their types contains `type`
+   * Return a list of {@link Clothe Clothe} objects when their types contains <code>type</code>
    */
   List<Clothe> findClothesByType(String type);
 
   /**
-   * Return a list of `Clothe` objects when their `bodies` attribute contains `bodyPart`
+   * Return a list of {@link Clothe Clothe} objects when their <code>bodies</code> attribute
+   * contains <code>bodyPart</code>
    * 
-   * @param bodyPart Bodypart is an Enum type which list all parts of the body available in the API
+   * @param bodyPart {@link Bodypart Bodypart} is an Enum type which list all parts of the body
+   *        available in the API
    */
   List<Clothe> findClothesByBodyPart(Bodypart bodyPart);
 
   /**
-   * Return a list of `Clothe` objects which contains `query` in at least one of the following
-   * attributes : model - brand - color - type - bodies
+   * Return a list of {@link Clothe Clothe} objects which contains <code>query</code> in at least
+   * one of the following attributes : model - brand - color - type - bodies
    */
   List<Clothe> findAll(String query);
 }
