c330a4f3d6a6842c34982e3ab7f85f0133175b34
blckshrk <alexand.bonhom@gmail.com>
2013-12-09 18:45:04 +0100

    DY-46 Test de la méthode StreamTools.convertStreamToString() => green

---
+++ src/test/java/fr/redteam/dressyourself/common/StreamToolsTest.java
@@ -0,0 +1,21 @@
+package fr.redteam.dressyourself.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+public class StreamToolsTest {
+
+  @Test
+  public void testConvertStreamToString() throws UnsupportedEncodingException {
+    String initialString = "abcd124//_\n\r#\nx)è";
+    InputStream is = new ByteArrayInputStream(initialString.getBytes("UTF-8"));
+    
+    String resultString = StreamTools.convertStreamToString(is);
+    assertEquals(initialString, resultString);
+  }
+}
