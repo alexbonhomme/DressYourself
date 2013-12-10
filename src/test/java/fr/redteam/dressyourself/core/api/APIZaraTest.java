e43d1b1a64048ad80e0605523c4c364b82ee808b
blckshrk <alexand.bonhom@gmail.com>
2013-12-10 22:44:46 +0100

    DY-46 MAJ des test dans APIZaraTest pour matcher avec les données de la nouvelles base de vêtements

--- src/test/java/fr/redteam/dressyourself/core/api/APIZaraTest.java
+++ src/test/java/fr/redteam/dressyourself/core/api/APIZaraTest.java
@@ -30,10 +30,10 @@ public class APIZaraTest {
     assertTrue(product != null);
 
     assertEquals(97, product.getId());
-    assertEquals("crossover cape jacket", product.getModel());
+    assertEquals("structured sweater", product.getModel());
     assertEquals("Zara", product.getBrand());
-    assertEquals("Black", product.getColor());
-    assertEquals("Sweat-shirt", product.getType());
+    assertEquals("Yellow", product.getColor());
+    assertEquals("Knitwears", product.getType());
     assertEquals("Top", product.getBodies());
 
     // TODO
@@ -44,35 +44,35 @@ public class APIZaraTest {
   public void testFindClothesByModel() {
     List<Clothe> listProducts = api.findClothesByModel("jeans");
 
-    assertEquals(92, listProducts.size());
+    assertEquals(93, listProducts.size());
   }
 
   @Test
   public void testFindClothesByType() {
     List<Clothe> listProducts = api.findClothesByType("sweat-shirt");
 
-    assertEquals(32, listProducts.size());
+    assertEquals(29, listProducts.size());
   }
 
   @Test
   public void testFindClothesByBodyPart() {
     List<Clothe> listProducts = api.findClothesByBodyPart(Bodypart.BOTTOM);
 
-    assertEquals(181, listProducts.size());
+    assertEquals(185, listProducts.size());
   }
 
   @Test
   public void testFindClothesByBrand() {
     List<Clothe> listProducts = api.findClothesByBrand("zara");
 
-    assertEquals(1008, listProducts.size());
+    assertEquals(1020, listProducts.size());
   }
 
   @Test
   public void testFindClothesByColor() {
     List<Clothe> listProducts = api.findClothesByColor("red");
 
-    assertEquals(17, listProducts.size());
+    assertEquals(16, listProducts.size());
   }
 
   @Test
