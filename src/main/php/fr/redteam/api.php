c4910f0b2f316e140c445623209529d144981bd3
blckshrk <alexand.bonhom@gmail.com>
2013-12-10 22:43:58 +0100

    DY-46 Définition d'une constante pour gérer les numéros de versions de la DB dans api.php

--- src/main/php/fr/redteam/api.php
+++ src/main/php/fr/redteam/api.php
@@ -1,9 +1,10 @@
 <?php
 require('dbhelper.php');
+define('DB_PATH', 'zara_20131210.sqlite');
 
 if (isset($_GET['q'])) {
 	$query = $_GET['q'];
-	$db = new DBHelper('zara_20131210.sqlite');
+	$db = new DBHelper(DB_PATH);
 	
 	if (isset($_GET['filters']) && !empty($_GET['filters'])) {
 		$filters = explode(",", $_GET['filters']);
@@ -16,7 +17,7 @@ if (isset($_GET['q'])) {
 }
 
 if (isset($_GET['id'])) {
-	$db = new DBHelper('zara_20131210.sqlite');
+	$db = new DBHelper(DB_PATH);
 	$json = $db->findClotheById($_GET['id']);
 	
 	exit($json);
