29c0f66c76cecb0a63948843b1ba18e3265a3aaa
dalarad <jerm8@free.fr>
2013-12-11 10:16:46 +0100

    DY-38: ActivityOutfitMailTest, création de la classe de test pour tester ActivityOutfitMail

---
+++ src/test/java/fr/redteam/dressyourself/activities/ActivityOutfitMailTest.java
@@ -0,0 +1,13 @@
+package fr.redteam.dressyourself.activities;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import fr.redteam.dressyourself.core.clothes.Outfit;
+
+@RunWith(RobolectricTestRunner.class)
+public class ActivityOutfitMailTest {
+  private ActivityOutfitMail activityOutfitMail;
+  private Outfit outfit;
+
+}
