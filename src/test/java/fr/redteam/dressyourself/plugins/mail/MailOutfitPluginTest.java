7d219aeca2cdf917fe7a55737454574de2019567
dalarad <jerm8@free.fr>
2013-12-17 23:29:49 +0100

    DY-48: Création de la classe MailOutfitPluginTest création de la classe de test de mailOutfitPlugin

---
+++ src/test/java/fr/redteam/dressyourself/plugins/mail/MailOutfitPluginTest.java
@@ -0,0 +1,10 @@
+package fr.redteam.dressyourself.plugins.mail;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MailOutfitPluginTest {
+
+
+}
