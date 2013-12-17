e7c74975519d5246aa39683812b85b8740e8cb18
blckshrk <alexand.bonhom@gmail.com>
2013-12-17 22:36:20 +0100

    DY-46 Amélioration de la couverture de test sur la classe APIAbstractHelper (test des cas d'erreur)

--- src/test/java/fr/redteam/dressyourself/core/api/APIZaraTest.java
+++ src/test/java/fr/redteam/dressyourself/core/api/APIZaraTest.java
@@ -2,13 +2,14 @@ package fr.redteam.dressyourself.core.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import org.junit.rules.ExpectedException;
 import fr.redteam.dressyourself.common.filemanager.FileManager;
 import fr.redteam.dressyourself.core.Bodypart;
 import fr.redteam.dressyourself.core.clothes.Clothe;
+import fr.redteam.dressyourself.exceptions.DressyourselfIOException;
 import fr.redteam.dressyourself.exceptions.DressyourselfRuntimeException;
 
 public class APIZaraTest {
@@ -45,7 +47,11 @@ public class APIZaraTest {
     String content = api.getContent(url);
     assertEquals("{\"key\": \"one\"}\n", content);
 
-    // Error cases
+    /*
+     * Error cases
+     * 
+     * Bad url
+     */
     url = new URL("http://www.thisisafakeurlwebsite.me");
 
     exception.expect(DressyourselfRuntimeException.class);
@@ -54,8 +60,44 @@ public class APIZaraTest {
   }
 
   @Test
-  public void testBuildClotheFromJSONArray() {
-    fail("Not yet implemented");
+  public void testBuildClotheFromJSONObject() throws JSONException {
+    JSONObject jsonClothe =
+        new JSONObject(
+            "{\"id\":876,"
+                + "\"model\":\"ankle boots with interior wedge and elastic panels\","
+                + "\"imageUrl\":\"http://static.zara.net/photos/2013/I/1/1/p/5155/201/003/2/w/400/5155201003_1_1_1.jpg?timestamp=1377247934735\","
+                + "\"imagePath\":\"Zara/en/woman/shoes/113-ankle_boots_with_interior_wedge_and_elastic_panels.jpg\","
+                + "\"brand\":\"Zara\"," + "\"color\":\"Ice\"," + "\"type\":\"Shoes\","
+                + "\"bodies\":\"Shoes\"}");
+
+    Clothe product = api.buildClotheFromJSONObject(jsonClothe);
+
+    assertTrue(product != null);
+    assertEquals(876, product.getId());
+    assertEquals("ankle boots with interior wedge and elastic panels", product.getModel());
+    assertEquals("Zara", product.getBrand());
+    assertEquals("Ice", product.getColor());
+    assertEquals("Shoes", product.getType());
+    assertEquals("Shoes", product.getBodies());
+    assertEquals("Zara/en/woman/shoes/113-ankle_boots_with_interior_wedge_and_elastic_panels.jpg",
+        product.getImageRelativePath());
+    
+    /*
+     * Error cases
+     * 
+     * Look at the `imageUrl` field
+     */
+    jsonClothe =
+        new JSONObject(
+            "{\"id\":876,"
+                + "\"model\":\"ankle boots with interior wedge and elastic panels\","
+                + "\"imageUrl\":\"http://thisisafakeurltotestanexception.ex\","
+                + "\"imagePath\":\"Zara/en/woman/shoes/113-ankle_boots_with_interior_wedge_and_elastic_panels.jpg\","
+                + "\"brand\":\"Zara\"," + "\"color\":\"Ice\"," + "\"type\":\"Shoes\","
+                + "\"bodies\":\"Shoes\"}");
+
+    exception.expect(DressyourselfIOException.class);
+    api.buildClotheFromJSONObject(jsonClothe);
   }
 
   /*
