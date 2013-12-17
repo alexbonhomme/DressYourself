b7b07321db92e77b01a9c1d118367ee009ba10e7
dalarad <jerm8@free.fr>
2013-12-17 23:36:41 +0100

    DY-48: MailOutfitPluginTest, implémentation de la méthode setUp.

--- src/test/java/fr/redteam/dressyourself/plugins/mail/MailOutfitPluginTest.java
+++ src/test/java/fr/redteam/dressyourself/plugins/mail/MailOutfitPluginTest.java
@@ -1,10 +1,40 @@
 package fr.redteam.dressyourself.plugins.mail;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import fr.redteam.dressyourself.activities.ActivityOutfitMail;
+import fr.redteam.dressyourself.core.clothes.Clothe;
+import fr.redteam.dressyourself.core.clothes.Outfit;
 
 @RunWith(RobolectricTestRunner.class)
 public class MailOutfitPluginTest {
+
+  private Activity activity;
+  private Outfit outfit;
+
+  @Before
+  public void setUp() throws Exception {
+    Outfit outfit = new Outfit();
+    Clothe clothe = new Clothe();
+    outfit.addClothe(clothe);
+    this.outfit = outfit;
+
+    Intent intent =
+        new Intent(Robolectric.getShadowApplication().getApplicationContext(),
+            ActivityOutfitMail.class);
+    Bundle bundle = new Bundle();
+    bundle.putSerializable("outfit", outfit);
+    intent.putExtras(bundle);
+    this.activity =
+        Robolectric.buildActivity(ActivityOutfitMail.class).withIntent(intent).create().visible()
+            .get();
+  }
 
 
 }
