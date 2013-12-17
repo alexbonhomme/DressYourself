77e697a9c14f82e365387e8e7516484fc54f8e32
dalarad <jerm8@free.fr>
2013-12-17 22:36:50 +0100

    DY-48: Création de la classe MailPluginTest pour tester MailPlugin

---
+++ src/test/java/fr/redteam/dressyourself/plugins/mail/MailPluginTest.java
@@ -0,0 +1,21 @@
+package fr.redteam.dressyourself.plugins.mail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import fr.redteam.dressyourself.activities.ActivityOutfitMail;
+import fr.redteam.dressyourself.core.clothes.Clothe;
+import fr.redteam.dressyourself.core.clothes.Outfit;
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class MailPluginTest {
+  
+
+}
