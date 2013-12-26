6142ab03e4a5863959ae9768c334b247274af405
blckshrk <alexand.bonhom@gmail.com>
2013-12-26 10:52:26 +0100

    Affchage des photos de vetement dans les listes. BUG certaines photos sont mal téléchargées. TO DO faire un system de cache au lieu de save toutes les photos.

--- src/main/java/fr/redteam/dressyourself/views/ListViewClothes.java
+++ src/main/java/fr/redteam/dressyourself/views/ListViewClothes.java
@@ -1,11 +1,17 @@
 package fr.redteam.dressyourself.views;
 
+import java.io.File;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import fr.redteam.dressyourself.R;
+import fr.redteam.dressyourself.common.filemanager.AndroidFileManager;
+import fr.redteam.dressyourself.core.ClothesManager;
 import fr.redteam.dressyourself.core.clothes.Clothe;
 
 /**
@@ -15,65 +21,65 @@ import fr.redteam.dressyourself.core.clothes.Clothe;
  */
 public class ListViewClothes extends LinearLayout {
 
-	private Clothe product;
+  private Clothe product;
 
-	private ImageView imageViewProduct;
-	private TextView textViewProductBrand;
-	private TextView textViewProductModel;
+  private ImageView imageViewProduct;
+  private TextView textViewProductBrand;
+  private TextView textViewProductModel;
 
-	public ListViewClothes(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		initComponent();
-	}
+  public ListViewClothes(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    initComponent();
+  }
 
-	public ListViewClothes(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initComponent();
-	}
+  public ListViewClothes(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    initComponent();
+  }
 
-	public ListViewClothes(Context context) {
-		super(context);
-		initComponent();
-	}
+  public ListViewClothes(Context context) {
+    super(context);
+    initComponent();
+  }
 
-	/**
-	 * Initialisation des composants
-	 */
-	private void initComponent() {
-		inflate(getContext(), R.layout.listview_clothes, this);
+  /**
+   * Initialisation des composants
+   */
+  private void initComponent() {
+    inflate(getContext(), R.layout.listview_clothes, this);
 
-		imageViewProduct = (ImageView) findViewById(R.id.imageViewAdapter);
-		textViewProductModel = (TextView) findViewById(R.id.textViewModelAdapter);
-		textViewProductBrand = (TextView) findViewById(R.id.textViewBrandAdapter);
-	}
+    imageViewProduct = (ImageView) findViewById(R.id.imageViewAdapter);
+    textViewProductModel = (TextView) findViewById(R.id.textViewModelAdapter);
+    textViewProductBrand = (TextView) findViewById(R.id.textViewBrandAdapter);
+  }
 
-	/**
-	 * Cette méthode "remplit" les élements de la vue avec les données issues d'un objet Clothe
-	 * 
-	 * @param product
-	 */
-	public void bind(Clothe product) {
-		this.product = product;
-		//TODO mettre image dynamique
-//		if (!(product.getImageRelativePath() == null)) {
-//			File imgFile = ClothesManager.loadClotheImage(getContext(), product.getImageRelativePath());
-//			if (imgFile.exists()) {
-//				Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-//				imageViewProduct.setImageBitmap(myBitmap);
-//			}
-//		}
-		imageViewProduct.setImageResource(R.raw.medium_wash_jeans);
-		textViewProductModel.setText(product.getModel());
-		textViewProductBrand.setText(product.getBrand());
-	}
+  /**
+   * Filled an element of the view with data from a {@link Clothe} object
+   */
+  public void bind(Clothe product) {
+    this.product = product;
 
-	/**
-	 * Retourn l'id du vêtement associé a cette élément
-	 * 
-	 * @return
-	 */
-	public Clothe getProduct() {
-		return product;
-	}
+    textViewProductModel.setText(product.getModel());
+    textViewProductBrand.setText(product.getBrand());
+
+    // if no image attached to this product, we just put a default image
+    if (product.getImageRelativePath() == null) {
+      imageViewProduct.setImageResource(R.drawable.logo_2_2_1);
+
+      return;
+    }
+
+    ClothesManager manager = new ClothesManager(new AndroidFileManager(getContext()));
+    File imgFile = manager.loadClotheImage(product.getImageRelativePath());
+
+    BitmapFactory.Options options = new BitmapFactory.Options();
+    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath(), options);
+    imageViewProduct.setImageBitmap(myBitmap);
+  }
+
+  public Clothe getProduct() {
+    return product;
+  }
 
 }
